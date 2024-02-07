cc5779f4e2eb5de8c9e98c0cc3f4a0768cec3bf7
Johannes <johannes.widkopp@gmail.com>
2024-02-07 09:00:48 +0200

    firebase harjoitus tehty

--- src/App.js
+++ src/App.js
@@ -1,23 +1,123 @@
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from 'firebase/auth';
+import { getFirestore, query, orderBy, limit, collection, addDoc, serverTimestamp, onSnapshot } from 'firebase/firestore';
+import { getAnalytics } from 'firebase/analytics';
+
+
+const firebaseConfig = {
+  //tähän tulee api- ja muut firebase avaimet 
+};
+
+
+initializeApp(firebaseConfig);
+const auth = getAuth();
+const firestore = getFirestore();
+getAnalytics();
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      setUser(user);
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Chatti</h1>
+        <SignOut />
       </header>
+
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
+    </div>
+  );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+  };
+
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+      <p>Do not violate the community guidelines or you will be banned for life!</p>
+    </>
+  );
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button className="sign-out" onClick={() => signOut(auth)}>Sign Out</button>
+  );
+}
+
+function ChatRoom() {
+  const dummy = useRef();
+  const [messages, setMessages] = useState([]);
+  const [formValue, setFormValue] = useState('');
+
+  useEffect(() => {
+    const q = query(collection(firestore, "messages"), orderBy("createdAt"), limit(25));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const messages = querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+      setMessages(messages);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await addDoc(collection(firestore, "messages"), {
+      text: formValue,
+      createdAt: serverTimestamp(),
+      uid,
+      photoURL
+    });
+
+    setFormValue('');
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  return (
+    <>
+      <main>
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+        <div ref={dummy}></div>
+      </main>
+
+      <form onSubmit={sendMessage}>
+        <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="write here" />
+        <button type="submit" disabled={!formValue}>send</button>
+      </form>
+    </>
+  );
+}
+
+function ChatMessage({ message }) {
+  const { text, uid, photoURL } = message;
+  const messageClass = uid === auth.currentUser?.uid ? 'sent' : 'received';
+
+  return (
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL} alt="Avatar" />
+      <p>{text}</p>
     </div>
   );
 }
